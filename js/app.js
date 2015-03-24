4340c5b6808e2f88ec4155334e4d7a4df1d97797
Robert Wilkinson <wilkinson.robert.a@gmail.com>
2015-03-24 14:35:08 -0700

    Adds player points function

--- js/app.js
+++ js/app.js
@@ -1,15 +1,33 @@
 window.onload = function () {
 	var circles = document.getElementsByClassName('hole');
-
+	var player = {
+		points: 0,
+		misses: 0
+	}
 // 1s after load, Mole appears in random hole
 function assignMole() {
 	var circle = circles[Math.floor(Math.random() * circles.length)];
 	circle.style.backgroundImage = "url(mole.png)";
 	circle.hasMole = true;
 }
+
 setInterval(assignMole,1000);
 
 // if player clicks on that mole he faints and player gains a point
+	for(var i = 0; i < circles.length; i++) {
+		var circle = circles[i];
+		console.log(circle);
+		circle.addEventListener('click', function() {
+			console.log("clicked");
+			if(this.hasMole === true) {
+				this.hasMole = false;
+				player.points++;
+				this.style.backgroundImage = "";
+				this.style.backgroundColor = "burlywood";
+			}
+			document.getElementById('points').innerHTML = player.points;
+		});
+	}
 
 // else he goes back under and misses is greater
 
